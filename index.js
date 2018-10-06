60321136e3f8469b7a5dec2fef2f8b3727b59baf
Katie Pennachio <katie.pennachio@gmail.com>
2018-10-06 15:47:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+function increaseRankBy(n) {
+  var rankedList = document.querySelectorAll('.ranked-list li');
+  for (let i = 0; i < rankedList.length; i++) {
+    rankedList[i].innerHTML = parseInt(rankedList[i].innerHTML) + n;
+  }
+}
+function deepestChild() {
+  const grandNode = document.querySelectorAll("#grand-node");
+  
+}
