fcf91af8398f53883f0a3dd73b48aa126c8ea501
Katie Pennachio <katie.pennachio@gmail.com>
2018-10-06 16:13:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+function increaseRankBy(n) {
+  var rankedList = document.querySelectorAll('.ranked-list li');
+  for (let i = 0; i < rankedList.length; i++) {
+    rankedList[i].innerHTML = parseInt(rankedList[i].innerHTML) + n;
+  }
+}
+function deepestChild() {
+  var grandNode = document.querySelectorAll('#grand-node div');
+  var last;
+      for (var i = 0; i < grandNode.length; i++) {
+       last = grandNode[i]; 
+      }
+  return last;
+}
+
+
+
+
